d303fef786a7da7cc946fa96f9ba3030be6ba6bb
blitterated <blitterated@proton.me>
2026-03-01 16:40:21 -0600

    Progress with table scraping

--- main.py
+++ main.py
@@ -8,14 +8,49 @@ def load_file_contents(file_name):
 def soupinate(html):
     return BeautifulSoup(html, "html.parser")
 
+def process_section_row(row):
+    #cell = row.find("td")
+    cell = row.td
+    #print(f"cell type: {type(cell)}, name: {cell.name}, content: {cell.text}")
+    return cell.text
+
+
+def process_tool_row(row):
+    name = row.find("td", class_="name-cell").text
+    home = row.find("td", class_="home-cell").text
+    docs = row.find("td", class_="doc-cell").text
+    repo = row.find("td", class_="repo-cell").text
+    lang = row.find("td", class_="lang-cell").text
+    desc = row.find("td", class_="desc-cell").text
+
+    tool_insert_sql_1 = "INSERT INTO tools (name, invocation, url_home, url_documentation, url_repository, description)"
+    tool_insert_sql_2 = f"VALUES (\"{name}\", \"{home}\", \"{docs}\", \"{repo}\", \"{desc}\");"
+
+    print(" ".join([tool_insert_sql_1, tool_insert_sql_2]))
+
+def process_rows(table_rows):
+    section_row = ""
+
+    for row in table_rows:
+        row_class = row['class'][0]
+        #print(f"row_class: {row_class}, type: {type(row_class)}")
+
+        match row_class:
+            case "section-row":
+                section_name = process_section_row(row)
+                print(f"section: {section_name}")
+
+            case "tool-row":
+                process_tool_row(row)
+
+
 def main():
     html = load_file_contents('terminal_tools_table.html')
     soup = soupinate(html)
 
     table_rows = soup.find_all("tr")
+    process_rows(table_rows)
 
-    for row in table_rows:
-        print(f"Row class: {row['class']}")
 
 
 if __name__ == "__main__":
