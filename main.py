b58951c5054bd87545b6d59d8e240faa494863ee
blitterated <blitterated@proton.me>
2026-03-01 14:42:49 -0600

    Try out html.parser

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
+from html.parser import HTMLParser
+
+current_tag = ""
+
+class ToolsHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        match tag:
+            case "tr":
+                print("Row Start")
+            case "th":
+                print("Header Cell Start")
+            case "td":
+                print("Cell Start")
+        #print("Encountered a start tag:", tag)
+
+    def handle_endtag(self, tag):
+        match tag:
+            case "tr":
+                print("Row End")
+            case "th":
+                print("Header Cell End")
+            case "td":
+                print("Cell End")
+        #print("Encountered an end tag :", tag)
+
+    def handle_data(self, data):
+        print("...", data)
+        #print("Encountered some data  :", data)
+
+parser = ToolsHTMLParser()
+
+with open('terminal_tools_table.html', 'r', encoding='utf-8') as file:
+    content = file.read()
+    parser.feed(content)
+
 def main():
     print("Hello from terminal-tools!")
 
