ca7d04dd4b78c2a07f9ccf9fd29c7c5f1cdb620f
blitterated <blitterated@proton.me>
2026-03-01 15:22:35 -0600

    Switch from HTMLParser to BeautifulSoup

--- main.py
+++ main.py
@@ -1,40 +1,21 @@
-from html.parser import HTMLParser
+from bs4 import BeautifulSoup
 
-current_tag = ""
+def load_file_contents(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        content = file.read()
+        return content
 
-class ToolsHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        match tag:
-            case "tr":
-                print("Row Start")
-            case "th":
-                print("Header Cell Start")
-            case "td":
-                print("Cell Start")
-        #print("Encountered a start tag:", tag)
-
-    def handle_endtag(self, tag):
-        match tag:
-            case "tr":
-                print("Row End")
-            case "th":
-                print("Header Cell End")
-            case "td":
-                print("Cell End")
-        #print("Encountered an end tag :", tag)
-
-    def handle_data(self, data):
-        print("...", data)
-        #print("Encountered some data  :", data)
-
-parser = ToolsHTMLParser()
-
-with open('terminal_tools_table.html', 'r', encoding='utf-8') as file:
-    content = file.read()
-    parser.feed(content)
+def soupinate(html):
+    return BeautifulSoup(html, "html.parser")
 
 def main():
-    print("Hello from terminal-tools!")
+    html = load_file_contents('terminal_tools_table.html')
+    soup = soupinate(html)
+
+    table_rows = soup.find_all("tr")
+
+    for row in table_rows:
+        print(row.text)
 
 
 if __name__ == "__main__":
