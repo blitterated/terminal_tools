45c46815f00c97f7596992c0346d76cf2b43192f
blitterated <blitterated@proton.me>
2026-03-01 15:24:09 -0600

    Add DDL script

---
+++ terminal_tools.sql
@@ -0,0 +1,32 @@
+CREATE TABLE tools (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  invocation TEXT NOT NULL,
+  url_home TEXT,
+  url_documentation TEXT,
+  url_repository TEXT,
+  description TEXT NOT NULL
+);
+
+CREATE TABLE tags (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE tools_tags_xref (
+  id INTEGER PRIMARY KEY,
+  tool_id INTEGER NOT NULL,
+  tag_id INTEGER NOT NULL,
+);
+
+CREATE TABLE languages (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE tools_languages_xref (
+  id INTEGER PRIMARY KEY,
+  tool_id INTEGER NOT NULL,
+  language_id INTEGER NOT NULL,
+  percentage REAL NOT NULL
+);
