eb95258027e0345e7be44b22b409fe60ba9b56f7
blitterated <blitterated@proton.me>
2026-03-16 18:08:01 -0500

    Add comments and run generated SQL against SQLite

--- run.sh
+++ run.sh
@@ -1,4 +1,17 @@
 #! /usr/bin/env bash
+
+# Delete generated files
 rm terminal_tools_data.sql
+rm terminal_tools.db
+
+# Generate SQL INSERTs
 uv run main.py > terminal_tools_data.sql
+
+# Display results in `bat`
 bat terminal_tools_data.sql
+
+# (Re)create database
+sqlite3 terminal_tools.db ".read terminal_tools_schema.sql"
+
+# Populate tables
+#sqlite3 terminal_tools.db ".read terminal_tools_data.sql"
