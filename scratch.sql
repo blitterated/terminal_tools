9660d60bc504f99a4e8e62e4b31641ff4b2c5552
blitterated <blitterated@proton.me>
2026-03-22 12:37:41 -0500

    Save SQL scratchpad file

---
+++ scratch.sql
@@ -0,0 +1,62 @@
+-- Show URLs and types by tool
+SELECT
+    t.name as "tool"
+  , ut.type as "link type"
+  , u.url as "link"
+FROM tool_urls u
+INNER JOIN tools t on t.id = u.tool_id
+INNER JOIN tool_url_types ut on ut.id = u.url_type_id
+ORDER BY t.name, ut.type;
+
+
+-- Show implementation languages by tool
+SELECT
+    t.name as "tool"
+  , tlx.percentage * 100 as "%"
+  , il.name as "lang"
+FROM tools t
+INNER JOIN tools_languages_xref tlx on t.id = tlx.tool_id
+INNER JOIN implementation_languages il on tlx.language_id = il.id
+ORDER BY t.name, tlx.percentage ;
+
+
+-- Show tools by section (a.k.a tag)
+SELECT
+    t.name as "tool"
+  , tg.name as "section"
+FROM tools t
+LEFT JOIN tools_tags_xref ttx on t.id = ttx.tool_id
+LEFT JOIN tags tg on ttx.tag_id = tg.id
+--WHERE tg.is_section = TRUE
+ORDER BY t.name;
+
+
+
+
+
+
+
+
+-- BOILERPLATE TEMPLATES
+
+INSERT INTO tags (name, is_section) VALUES ('', TRUE);
+
+
+INSERT INTO tools (
+    name,
+    invocation,
+    url_home,
+    url_documentation,
+    url_repository,
+    implementation_language,
+    description
+)
+VALUES (
+    '',
+    NULL,
+    '',
+    '',
+    '',
+    '',
+    ''
+);
