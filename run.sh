1cfcbd1a6096907482a8bd300d360be3fe643eed
blitterated <blitterated@proton.me>
2026-03-22 12:35:39 -0500

    Swap comments for echoes. Improve file deletion.

--- run.sh
+++ run.sh
@@ -1,17 +1,33 @@
 #! /usr/bin/env bash
 
-# Delete generated files
-rm terminal_tools_data.sql
-rm terminal_tools.db
+# Fail script on any error [-e].
+# Fail script on unassigned variable usage [-u].
+set -eu
 
-# Generate SQL INSERTs
+rm_ephemeral () {
+  filename="${1}"
+  if [ -f "${filename}" ]; then
+    echo "Delete generated ${filename}."
+    rm "${filename}"
+  else
+    echo "No generated ${filename} found."
+  fi
+}
+
+rm_ephemeral "./terminal_tools_data.sql"
+rm_ephemeral "./terminal_tools.db"
+
+echo "Generate SQL INSERTs"
 uv run main.py > terminal_tools_data.sql
 
 # Display results in `bat`
-bat terminal_tools_data.sql
+#bat terminal_tools_data.sql
 
-# (Re)create database
+echo "(Re)create database"
 sqlite3 terminal_tools.db ".read terminal_tools_schema.sql"
 
-# Populate tables
+echo "Populate tables"
 sqlite3 terminal_tools.db ".read terminal_tools_data.sql"
+
+echo "Hack in tools to languages cross refs"
+sqlite3 terminal_tools.db ".read tools_languages_xref.sql"
