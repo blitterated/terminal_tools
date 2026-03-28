0c4fb7954f0c4dca8885d1bb944e39abeb8cbae4
blitterated <blitterated@proton.me>
2026-03-28 12:25:44 -0500

    Capitalize "AS"

--- scratch.sql
+++ scratch.sql
@@ -1,8 +1,8 @@
 -- Tools and URLs
 SELECT
-    t.name as "tool"
-  , ut.type as "link type"
-  , u.url as "link"
+    t.name AS "tool"
+  , ut.type AS "link type"
+  , u.url AS "link"
 FROM tool_urls u
 INNER JOIN tools t on t.id = u.tool_id
 INNER JOIN tool_url_types ut on ut.id = u.url_type_id
@@ -11,9 +11,9 @@ ORDER BY t.name, ut.type;
 
 -- Tools and Languages
 SELECT
-    t.name as "tool"
-  , tlx.percentage * 100 as "%"
-  , il.name as "lang"
+    t.name AS "tool"
+  , tlx.percentage * 100 AS "%"
+  , il.name AS "lang"
 FROM tools t
 INNER JOIN tools_languages_xref tlx on t.id = tlx.tool_id
 INNER JOIN implementation_languages il on tlx.language_id = il.id
@@ -22,8 +22,8 @@ ORDER BY t.name, tlx.percentage ;
 
 -- Tools by Section (a.k.a Tags)
 SELECT
-    t.name as "tool"
-  , COALESCE(tg.name, '(default)') as "section"
+    t.name AS "tool"
+  , COALESCE(tg.name, '(default)') AS "section"
 FROM tools t
 LEFT JOIN tools_tags_xref ttx on t.id = ttx.tool_id
 LEFT JOIN tags tg on ttx.tag_id = tg.id
@@ -34,10 +34,10 @@ ORDER BY tg.name, t.name;
 
 -- Tool IDs, names, invocations, and repos
 SELECT
-    t.id as "tool id"
-  , t.name as "tool name"
-  , t.invocation as "invocation"
-  , u.url as "repository"
+    t.id AS "tool id"
+  , t.name AS "tool name"
+  , t.invocation AS "invocation"
+  , u.url AS "repository"
 FROM tools t
 INNER JOIN tool_urls u on t.id = u.tool_id
 INNER JOIN tool_url_types ut on ut.id = u.url_type_id
