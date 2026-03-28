20cade29c943c8ce2b78bca3119a06560be186a3
blitterated <blitterated@proton.me>
2026-03-28 15:49:37 -0500

    Factor boilerplate SQL into separate file
    Wrap with a transaction as a reminder.

---
+++ boilerplate.sql
@@ -0,0 +1,71 @@
+-- BOILERPLATE SQL TEMPLATES
+
+BEGIN TRANSACTION;
+
+INSERT INTO tags (name, is_section) VALUES ('', TRUE);
+
+INSERT INTO tools (
+    name,
+    invocation,
+    description
+)
+VALUES (
+    '', -- name
+    '', -- invocation
+    ''  -- description
+);
+
+INSERT INTO tool_urls (
+    tool_id,
+    url_type_id,
+    url
+)
+VALUES (
+    (SELECT id from tools WHERE name = ''), -- tool_id
+    (SELECT id from tool_url_types WHERE name = 'Repository'), -- url_type_id
+    '' -- url
+);
+
+INSERT INTO tool_urls (
+    tool_id,
+    url_type_id,
+    url
+)
+VALUES (
+    (SELECT id from tools WHERE name = ''), -- tool_id
+    (SELECT id from tool_url_types WHERE name = 'Homepage'), -- url_type_id
+    '' -- url
+);
+
+INSERT INTO tool_urls (
+    tool_id,
+    url_type_id,
+    url
+)
+VALUES (
+    (SELECT id from tools WHERE name = ''), -- tool_id
+    (SELECT id from tool_url_types WHERE name = 'Documentation'), -- url_type_id
+    '' -- url
+);
+
+INSERT INTO tools_tags_xref (
+    tool_id,
+    tag_id
+)
+VALUES (
+    (SELECT id from tools WHERE name = ''), -- tool_id
+    (SELECT id from tags WHERE name = ''), -- tag_id
+);
+
+INSERT INTO tools_languages_xref (
+    tool_id,
+    language_id,
+    percentage
+)
+VALUES (
+    (SELECT id from tools WHERE name = ''), -- tool_id
+    (SELECT id from implementation_languages WHERE name = ''), -- language_id
+    1.0  -- percentage
+);
+
+COMMIT;
