8c90bf5969055d70b5dadff7d34e847d76488d8c
blitterated <blitterated@proton.me>
2026-03-28 15:23:42 -0500

    Add remaining boilerplate INSERTs

--- scratch.sql
+++ scratch.sql
@@ -68,3 +68,56 @@ VALUES (
     '', -- invocation
     ''  -- description
 );
+
+INSERT INTO tool_urls (
+    tool_id,
+    url_type_id,
+    url
+)
+VALUES (
+    (SELECT id from tools WHERE name = ''), -- tool_id
+    (SELECT id from tool_url_typs WHERE name = 'Repository'), -- url_type_id
+    '' -- url
+);
+
+INSERT INTO tool_urls (
+    tool_id,
+    url_type_id,
+    url
+)
+VALUES (
+    (SELECT id from tools WHERE name = ''), -- tool_id
+    (SELECT id from tool_url_typs WHERE name = 'Homepage'), -- url_type_id
+    '' -- url
+);
+
+INSERT INTO tool_urls (
+    tool_id,
+    url_type_id,
+    url
+)
+VALUES (
+    (SELECT id from tools WHERE name = ''), -- tool_id
+    (SELECT id from tool_url_typs WHERE name = 'Documentation'), -- url_type_id
+    '' -- url
+);
+
+INSERT INTO tools_tags_xref (
+    tool_id,
+    tag_id
+)
+VALUES (
+    (SELECT id from tools WHERE name = ''), -- tool_id
+    (SELECT id from tags WHERE name = ''), -- tag_id
+);
+
+INSERT INTO tools_languages_xref (
+    tool_id,
+    language_id,
+    percentage
+)
+VALUES (
+    (SELECT id from tools WHERE name = ''), -- tool_id
+    (SELECT id from implementation_languages WHERE name = ''), -- language_id
+    1.0  -- percentage
+);
