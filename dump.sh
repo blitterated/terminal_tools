c136f4c3341f0be6e47e6c3f0250ec93d9c95494
blitterated <blitterated@proton.me>
2026-04-01 12:59:27 -0500

    Add a single version backup file. Parameterize filenames.

--- dump.sh
+++ dump.sh
@@ -1,4 +1,17 @@
 #! /usr/bin/env bash
+
+dump_filename="terminal_tools_dump.sql"
+backup_filename="${dumpfile}.bak"
+
+# Delete old backup
+if [ -f "${backup_filename}" ]; then
+  rm "${backup_filename}"
+fi
+
+# Back up existing dump file script
+mv "${dump_filename}" "${backup_filename}"
+
+# Dump the database to SQL
 sqlite3 terminal_tools.db .dump | \
-tee terminal_tools_dump.sql | \
+tee "${dump_filename}" | \
 bat -l sql
