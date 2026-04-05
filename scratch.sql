d433956424fca104427bbe183589429c9a02026d
blitterated <blitterated@proton.me>
2026-04-05 15:36:47 -0500

    Add queries for tools and sections

--- scratch.sql
+++ scratch.sql
@@ -43,3 +43,85 @@ INNER JOIN tool_urls u on t.id = u.tool_id
 INNER JOIN tool_url_types ut on ut.id = u.url_type_id
 WHERE ut.type = 'Repository'
 ORDER BY LOWER(t.name);
+
+
+-- List sections alphabetically
+SELECT DISTINCT tg.name
+FROM tags tg
+INNER JOIN tools_sections ts ON tg.id = ts.tool_id 
+ORDER BY LOWER(tg.name);
+
+
+SELECT t.name, t.description 
+FROM tools t
+ORDER BY LOWER(t.name);
+
+
+
+-- Dev Tooling for the Linux and macOS Terminal.md
+--------------------------------------------------------------------------------
+-- timeout - automatically stop long running commands
+-- trash - sends files to OS' trash can instead of perma-deleting
+-- gping - Graphical ping.
+-- dasel - JSON, YAML, TOML, CSV TUI.
+-- qsv - CSV for big files.
+-- grex - Generate regexes from samples.
+-- sq - Query SQL, JSON, SQL, Excel.
+-- croc - Secure file sharing/sending.
+-- FastFetch - NeoFetch clone.
+-- Inav - Log file navigator and query tool.
+-- sops - Encrypt/decrypt values only in a JSON, YAML, TOML, or .env file.
+-- bottom - Yet another cross-platform graphical process/system monitor.
+-- tokei - Count your code, quickly.
+-- doggo - Command-line DNS Client for Humans.
+-- fx - Terminal JSON viewer & processor
+-- gum - A tool for glamorous shell scripts
+-- lsd - The next gen ls command
+-- viddy - A modern watch command.
+-- duf - Disk Usage/Free Utility - a better ‘df’ alternative
+-- resterm - Terminal API client for HTTP/GraphQL/gRPC
+-- harlequin - The SQL IDE for Your Terminal
+-- jj - Jujutsu. A Git-compatible VCS that is both simple and powerful
+-- tailspin - A log file highlighter
+-- lazydocker - The lazier way to manage everything docker
+-- superfile - Pretty fancy and modern terminal file manager
+-- hexly - A command-line hex viewer
+-- taskwarrior-tui - taskwarrior-tui: A terminal user interface for taskwarrior
+-- helix - A post-modern modal text editor.
+-- age - A simple, modern and secure encryption tool
+-- trippy - A network diagnostic tool
+-- broot - Broot is a better way to navigate directories, find files, and launch commands.
+-- ouch - Painless compression and decompression in the terminal
+-- jless - jless is a command-line JSON viewer designed for reading, exploring, and searching through JSON data.
+-- procs - A modern replacement for ps written in Rust
+-- vd - VisiData is an interactive multitool for tabular data.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
